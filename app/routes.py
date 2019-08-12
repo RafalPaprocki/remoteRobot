9c414623126009bd972df885a065b494cfaa1ecb
Rafal Paprocki <rafalpaprocki@o2.pl>
2019-08-12 20:36:34 +0200

    urls for navbar items

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,19 @@ from app import app
 @app.route('/')
 def index():
     return render_template('mainPage.html')
+
+
+@app.route('/robot-config')
+def robot_config():
+    return render_template('robotConfig.html')
+
+
+@app.route('/robot-routes')
+def robot_routes():
+    return render_template('robotRoutes.html')
+
+
+@app.route('/data-preview')
+def data_preview():
+    return render_template('dataPreview.html')
+
